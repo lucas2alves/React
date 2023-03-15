eddcf6e71d8a19b23d8a847bd7fcc4f6346bfdef
lucas2alves <lucasalvesnascimento2010@hotmail.com>
2023-03-15 15:59:27 -0300

    estilizando-formulario-css

--- formulario/script.js
+++ formulario/script.js
@@ -7,14 +7,14 @@ btn.addEventListener("click", function(e) {
 
     const name = document.querySelector("#name");
     const valuename = name.value;
-    document.querySelector("#textname").innerHTML = valuename;
+    document.querySelector("#textname").innerHTML ="Name: " + valuename;
 
     const email = document.querySelector("#email");
     const valueemail = email.value;
-    document.querySelector("#textemail").innerHTML = valueemail;
+    document.querySelector("#textemail").innerHTML = "Email: " + valueemail;
 
     const tel = document.querySelector("#tel");
     const valuetel = tel.value;
-    document.querySelector("#texttel").innerHTML = valuetel;
+    document.querySelector("#texttel").innerHTML = "Tel: " + valuetel;
 
 });
