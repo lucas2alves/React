e5362ee29e08a7e98d4618077aeb379bcd9480e6
lucas2alves <lucasalvesnascimento2010@hotmail.com>
2023-04-17 13:06:03 -0300

    finalizando-projeto

--- formulario/script.js
+++ formulario/script.js
@@ -1,17 +1,70 @@
 
-var formulario = document.querySelector('form');
+var formulario = document.querySelector('input');
 const btn = document.querySelector('#send');
+let check = document.querySelector("#confirmo");
+var select = document.querySelector('#estados');
+var form = document.querySelector("#form");
+var dadostela = document.querySelector("#dadostela");
+var html = `<p id="dadostela">Dados do usuario: <br> </p>`;
 
 
-formulario.onsubmit = function(){
+btn.onclick = function(){
+
    if(!document.querySelector("input").value){
 
-      alert("Preencha corretamente!");
+      alert("Preencha seu nome!");
+      btn.innerHTML = "Enviar";
+      return false;
+      
+   }
+   
+
+
+   if(!document.querySelector("#estados").value){
+
+      alert("Selecione um Estado!");
+      btn.innerHTML = "Enviar";
       return false;
       
    }
 
+   if(!document.querySelector("#confirmo").checked){
+
+      alert("Confirme antes de enviar");
+      btn.innerHTML = "Enviar";
+      return false;
+   }
+
+   dadostela.innerHTML += "Nome- " + formulario.value + " / " + "Estado- " + select.value + " / " + check.value + html; ;
+   btn.innerHTML = "Sucesso";
    alert ("Formulario enviado com sucesso");
-   
+   return false;
+  
 }
 
+
+formulario.onclick = function(e){
+   if(!document.querySelector("input").value){
+
+      btn.innerHTML = "Enviar";
+      select.checked = false;
+      return false;
+
+     
+   }
+
+    btn.innerHTML = "Enviar";
+   return false;
+}
+
+select.onclick = function(e){
+   if(!document.querySelector("#estados").value){
+
+      btn.innerHTML = "Enviar";
+      return false;
+     
+   }
+    btn.innerHTML = "Enviar";
+   return false;
+
+}
